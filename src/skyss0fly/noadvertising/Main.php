557af3e017b52aeac25eb29616b5d7a48f669ef5
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-31 20:17:10 +1000

    Update Main.php

--- src/skyss0fly/noadvertising/Main.php
+++ src/skyss0fly/noadvertising/Main.php
@@ -49,7 +49,7 @@ public function onChat(PlayerChatEvent $event) {
     $l = $this->getConfig()->get("Enabled");
   
   if($l) {
-      if ($chat->str_contains($bannedmessage)){
+      if (str_contains($bannedmessage, $chat)){
 $player->sendMessage($message);
           }
   else{
