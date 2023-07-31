b84759d733e04ed091390da2a9803a561ca6d401
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-31 20:06:18 +1000

    Create Main.php

---
+++ src/skyss0fly/noadvertising/Main.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace skyss0fly\noadvertising;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\player\Player;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
+
+class Main extends PluginBase implements Listener {
+
+public function onLoad(): void {
+$this->savedefaultconfig();
+}
+  public function onEnable(): void {
+$this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+    
+  }
+
+public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
+  switch ($command->getName()) {
+            case "disable":
+$message = $this->getConfig()->get("Message");
+$value = $this->getConfig()->get("Enabled");
+  if ($value) {
+  $this->setfalse();
+  $sender->sendMessage("Disabled the moderation");
+  
+}
+  else {
+$this->settrue();
+  $sender->sendMessage("Enabled the moderation");
+  
+  }
+  
+}
+  return true;
+}
+
+  
+public function onChat(PlayerChatEvent $event) {
+        $player = $event->getPlayer();
+        $chat = $event->getMessage();
+  $message = $this->getConfig()->get("Message");
+    $l = $this->getConfig()->get("Enabled");
+  
+  if($l) {
+      if ($chat->str_contains($bannedmessage){
+$player->sendMessage($message);
+          }
+  else{
+
+  }
+}
+  else {
+
+  // nothing lol
+  }
+}
+ public function settrue(): void {
+   $this->getConfig()->set("Enabled", true);
+        $this->getConfig()->save();
+    }
+  public function setfalse(): void {
+    $this->getConfig()->set("Enabled", False);
+        $this->getConfig()->save();
+  }
+}
